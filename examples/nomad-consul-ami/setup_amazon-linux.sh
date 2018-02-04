b73d2c04ef342747f61618b4b81bcb81ee2b7742
Matthias Scholz <matthias.scholz@gmail.com>
2018-02-04 17:36:18 +0100

    .NEW Support for Amazon Linux 2
    .REFACTOR supervisord usage in order to use systemd for Nomad startup
    .NEW add supervisord systemd startup

--- examples/nomad-consul-ami/setup_amazon-linux.sh
+++ examples/nomad-consul-ami/setup_amazon-linux.sh
@@ -8,5 +8,6 @@ sudo yum install -y git
 
 echo "[INFO] [${SCRIPT}] Setup docker"
 sudo yum install -y docker
-sudo service docker start
+sudo systemctl enable docker
+# TODO: Check if this is also the user nomad uses
 sudo usermod -a -G docker ec2-user
--- examples/nomad-consul-ami/setup_nomad_consul.sh
+++ examples/nomad-consul-ami/setup_nomad_consul.sh
@@ -4,5 +4,6 @@ set -e
 # Environment variables are set by packer
 /tmp/terraform-aws-nomad/modules/install-nomad/install-nomad --version "${NOMAD_VERSION}"
 
+# FIXME: Removed supervisord usage from consul as well
 git clone --branch "${CONSUL_MODULE_VERSION}"  https://github.com/hashicorp/terraform-aws-consul.git /tmp/terraform-aws-consul
 /tmp/terraform-aws-consul/modules/install-consul/install-consul --version "${CONSUL_VERSION}"
